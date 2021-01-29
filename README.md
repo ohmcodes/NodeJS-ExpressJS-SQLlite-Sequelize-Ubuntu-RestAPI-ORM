5893a3a015e9a27558b98ca03419046408b54452
ohmcodes <ohmcodes@gmail.com>
2021-01-29 03:18:45 -0800

    newtuts

--- README.md
+++ README.md
@@ -1,2 +1,102 @@
 # NodeJS-ExpressJS-SQLlite-Sequelize-Ubuntu-RestAPI-ORM
 NodeJS-ExpressJS-SQLlite-Sequelize-Ubuntu-RestAPI-ORM
+
+This Repo will teach you how to Create RESTAPI using NodeJS ExpressJS Sequelize SQLite and Ubunt20, it has CRUD function as well and ORM for queries.
+
+Here's the orignal video (must watch if you are iOS user):
+https://www.youtube.com/watch?v=VDgXAw7VynQ&feature=emb_logo&ab_channel=DavidTang
+
+## Requirements
+    - NodeJS
+    - ExpressJS
+    - SQLite3
+    - Sequelize
+    - Ubuntu20
+    - Nodemon
+    - Unzip
+    - body-parser
+    - Postman DL(https://www.postman.com/downloads/)
+
+## Install
+```
+npm i express sequelize sqlite3 body-parser --save
+npm install --save-dev nodemon
+```
+
+## Start nodemod
+```
+npx nodemon app.js
+```
+
+## Setup SQLite3
+```
+sudo apt-get install sqlite3 libsqlite3-dev
+
+Create db
+sqlite3 ness.db
+
+To SAVE
+.save ness.db
+
+To QUIT
+.quit
+```
+
+## Download sample Database
+```
+https://www.sqlitetutorial.net/sqlite-sample-database/
+or 
+
+wget https://cdn.sqlitetutorial.net/wp-content/uploads/2018/03/chinook.zip
+
+Install Unzip
+sudo apt install unzip
+
+UNZIP the file
+unzip chinook.zip
+
+Delete the source
+sudo rm chinook.zip
+```
+
+## Create directory
+```
+mkdir models
+mkdir database
+```
+
+## Endpoints
+```
+//GET
+localhost:8000/api/playlists
+localhost:8000/api/playlists/:id
+localhost:8000/api/tracks/:id
+localhost:8000/api/artists
+localhost:8000/api/artists/:id
+localhost:8000/api/albums/:id
+```
+
+## POSTMAN (CREATE, UPDATE and DELETE)
+Note: this endpoints is post type
+```
+//UPDATE
+//POST: localhost:8000/api/artists/
+//Body (raw)
+{
+    "id": 2,
+    "name" "Acceptable"
+}
+
+//CREATE
+//POST: localhost:8000/api/create/artists
+//Body (raw)
+{
+    "name" "New artist"
+}
+```
+
+Note: this endpoint is direct function
+```
+//DELETE
+localhost:8000/api/playlists/:id
+```
---
+++ app.js
@@ -0,0 +1,212 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const Playlist = require('./models/playlist');
+const Artist = require('./models/artist');
+const Album = require('./models/album');
+const Track = require('./models/track');
+const Sequelize = require('sequelize');
+
+const { Op } = Sequelize;
+
+const app = express();
+
+app.use(bodyParser.json());
+
+//RELATIONSHIPS
+Artist.hasMany(Album, {
+    foreignKey: 'ArtistId'
+});
+
+Album.belongsTo(Artist, {
+    foreignKey: 'ArtistId'
+});
+
+Playlist.belongsToMany(Track, {
+    through: 'playlist_track',
+    foreignKey: 'PlaylistId',
+    timestamps: false
+});
+
+Track.belongsToMany(Playlist, {
+    through: 'playlist_track',
+    foreignKey: 'TrackId',
+    timestamps: false
+});
+//END OF RELATIONSHIPS
+
+
+//POST API (UPDATE)
+app.post('/api/update/artists', function(request,response) {
+    Artist.update({
+        name: request.body.name
+    }, { 
+        where: {
+            id: request.body.id
+        }
+    }).then((artist) => {
+        response.json(artist);
+    }, (validation) => {
+        //raw
+        //response.json(errors);
+
+        response.status(422).json({
+            errors: validation.errors.map((error) => {
+                //only msg
+                //return error.message
+
+                return {
+                    attribute: error.path,
+                    message: error.message
+                }
+            })
+        });
+    });
+});
+
+//DELETE API
+app.delete('/api/playlists/:id', function(request, response) {
+    let { id } = request.params;
+
+    Playlist
+    .findByPk(id)
+    .then((playlist) =>{
+        if(playlist) {
+            return playlist.setTracks([]).then(()=> {
+                return playlist.destroy();
+            });
+        } else {
+            return Promise.reject();
+        }
+    })
+    .then(() => {
+        response.status(204).send();
+    }, () => {
+        response.status(404).send();
+    });
+});
+
+//POST API (CREATE)
+app.post('/api/create/artists', function(request,response) {
+    Artist.create({
+        name: request.body.name
+    }).then((artist) => {
+        response.json(artist);
+    }, (validation) => {
+        //raw
+        //response.json(errors);
+
+        response.status(422).json({
+            errors: validation.errors.map((error) => {
+                //only msg
+                //return error.message
+
+                return {
+                    attribute: error.path,
+                    message: error.message
+                }
+            })
+        });
+    });
+});
+
+//END POST API
+
+
+//GET API
+app.get('/api/playlists', function(request,response) {
+    let filter = {};
+    let { q } = request.query;
+    if(q){
+        filter = {
+            where: {
+                name: {
+                    [Op.like]: `${q}%`
+                }
+            }
+        }
+    }
+    
+    
+    Playlist.findAll(filter).then((playlists) => {
+        response.json(playlists);
+    });
+});
+
+app.get('/api/playlists/:id', function(request,response) {
+    let { id } = request.params;
+    Playlist.findByPk(id, {
+        include: [Track]
+    }).then((playlists) => {
+        if(playlists){
+            response.json(playlists);
+        } else {
+            response.status(404).send();
+        }
+    });
+});
+
+app.get('/api/tracks/:id', function(request,response) {
+    let { id } = request.params;
+    Track.findByPk(id, {
+        include: [Playlist]
+    }).then((track) => {
+        if(track){
+            response.json(track);
+        } else {
+            response.status(404).send();
+        }
+    });
+});
+
+app.get('/api/artists/:id', function(request,response) {
+    let { id } = request.params;
+    Artist.findByPk(id, {
+        include: [Album]
+    }).then((artist) => {
+        if(artist){
+            response.json(artist);
+        } else {
+            response.status(404).send();
+        }
+    });
+});
+
+app.get('/api/artists', function(request,response) {
+    let filter = {};
+    let { q } = request.query;
+    if(q){
+        filter = {
+            where: {
+                name: {
+                    [Op.like]: `${q}%`
+                }
+            }
+        }
+    }
+    
+    
+    Artist.findAll(filter).then((artist) => {
+        response.json(artist);
+    });
+});
+
+app.get('/api/albums/:id', function(request,response) {
+    let { id } = request.params;
+    Album.findByPk(id, {
+        include: [Artist]
+    }).then((album) => {
+        if(album){
+            response.json(album);
+        } else {
+            response.status(404).send();
+        }
+    });
+});
+//END GET API
+
+
+
+app.listen(8000);
+
+
+
